b9633ff38ca2f1365f1a5ad1870e51e3409fc408
PZL <c17196120@sina.com>
2015-11-30 10:10:16 +0800

    尝试加入地雷生成代码，发现问题问题

--- ScanBomb.c
+++ ScanBomb.c
@@ -1,39 +1,52 @@
 #include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#include <windows.h>
 #define N 10
+
+int random(int m, int n)
+{
+	srand((unsigned)time(0));
+	m = rand() % N +1;
+	n = rand() % N +1;
+	//Sleep(200);
+	return m, n;
+}
+
 int main()
 {
-	int x, y, i = N-1, j = N-1;
+	int x, y, i, j, k;
+	int m[N], n[N];
 	char b[N][N] = {0};
 	/*for (; i > 0; i--)
 		for (; j > 0; j--)
 			b[i][j] = 'o';
  	printf("请输入雷区范围（x < 21，y < 21）：");   //输入雷区范围用代码
 	scanf("%d %d", &x, &y);*/
-	for (; i > 0; i--)
+
+	/*for (i = 0; i < N; i++)
 	{
-		for (j = N-1; j > 0; j--)
+		for (j = 0; j < N; j++)
 		{
 		b[i][j] = '*';
 		}
-		b[i][N-1] = '\0';
 	}
-		
-		
-	/*srand((unsigned)time(0));
-	for (x = 9; x > 0; x--)
-		for (y = 9; y > 0; y--){
-		z = rand() % 10 + 1;
-		if (z > 8)
-			b[x][y] = 'B';
-		}*/
 	
-	for (x = N-1; x > 0; x--)
+	for (x = 0; x < N; x++)
 	{
-		for (y = N-1; y > 0; y--)
+		for (y = 0; y <N; y++)
 			printf("%c", b[x][y]);
-		//b[x][y] = '\0';
 		printf("\n");
+	}*/
+
+	for (k = 0; k < 10; k++)
+	{
+		m[k] = random(m[k], n[k]);
+		n[k] = random(m[k], n[k]);
 	}
-		
-		
+	for (k = 0; k < 10; k++)
+	{
+		printf("%d%d\n", m[k], n[k]);
+	}
+	
 }
